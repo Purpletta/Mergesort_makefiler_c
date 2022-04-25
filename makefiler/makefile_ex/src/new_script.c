fcbb42002aa15c7365202c8908cbcc3e50bff33f
Purpletta <101121914+Purpletta@users.noreply.github.com>
2022-04-25 17:44:30 +0300

    Delete makefiler/makefile_ex directory

--- makefiler/makefile_ex/src/new_script.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "../import/functions.h"
-
-void print_text() {
-    printf("it`s a script!");
-}
--- makefiler/makefile_ex/src/sum.c
+++
@@ -1,9 +0,0 @@
-#include "../import/functions.h"
-
-int sum(int n) {
-    int summary;
-    for (int k = 1; k != n; k++) {
-        summary += j;
-    }
-    return summary;
-}
